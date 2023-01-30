8f68a2173c83623333ce5e71c4568a854fe1abea
heavyblack <heavyblack1@gmail.com>
2023-01-30 17:07:55 +0100

    small formatting fix

--- app/question_detail.php
+++ app/question_detail.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add answer</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-
     <?php
     require "db_connect_conf.php";
     if (isset($_POST["text"])) {
@@ -41,6 +42,7 @@
         $conn->close();
     }
     ?>
+    <section>
     <?php
     require "db_connect_conf.php";
 
@@ -73,7 +75,6 @@
     }
     $conn->close();
     ?>
-    <section>
         <h2>Answer:</h2>
         <form method="POST" class="form-block">
             <label for="text-answer">Write answer:</label>
@@ -81,8 +82,6 @@
             <input type="submit" value="Insert">
         </form>
     </section>
-
-    <a href="index.php">Back</a>
+    <footer><a href="index.php">Back</a></footer>
 </body>
-
 </html>
